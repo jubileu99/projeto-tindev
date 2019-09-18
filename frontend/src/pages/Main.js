f64de66381c1bee53c4e7fe1f35c215421b21d0d
Victor Bernardes <victor_luizbernardes5@outlook.com>
2019-09-18 00:52:35 -0300

    update

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import './Main.css';
 
-export default function Main(){
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
+
+export default function Main({ match }){
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers:{
+                    user: match.params.id,
+                }
+            })
+            console.log(response.data);
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [match.params.id]);
+
     return(
-        <h1>DEBUGGER ATIVADO $$</h1>
+       <div className="main-container">
+           <img src={logo} alt="tindev"/>
+           <ul>
+               {users.map( user => (
+                <li>
+                   <img src={user.avatar} alt=""/>
+                <footer>
+                   <strong>user.name</strong>
+                   <p>user.bio</p>
+                </footer>
+                <div className="buttons">
+                   <button type="button">
+                       <img src={dislike} alt=""/> 
+                   </button>
+                   <button type="button">
+                       <img src={like} alt=""/>
+                   </button>
+                </div>
+                </li>
+               ))}
+           </ul>
+       </div>
     );
 }
